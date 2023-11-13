acd99ac82bdad31a1f7c9bbc4e6a327d2e34877b
Hicaro Bauer <hicarononato@gmail.com>
2023-11-12 23:51:02 -0300

    ImageDTO adicionado

--- src/main/java/io/github/HicaroBauer/imageliteapi/application/images/ImageMapper.java
+++ src/main/java/io/github/HicaroBauer/imageliteapi/application/images/ImageMapper.java
@@ -22,4 +22,14 @@ public class ImageMapper {
                 .build();
     }
 
+    public ImageDTO imageToDTO(Image image, String url){
+        return ImageDTO.builder()
+                .url(url)
+                .extension(image.getExtension().name())
+                .name(image.getName())
+                .size(image.getSize())
+                .uploadDate(image.getUploadDate().toLocalDate())
+                .build();
+    }
+
 }
